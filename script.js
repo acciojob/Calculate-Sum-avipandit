ac683c4e36c4cbfd374affba22d07ac749e36323
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-04-17 22:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 function add(){
 	 num1=parseInt(document.getElementById("n1").value);
      num2=parseInt(document.getElementById("n2").value);
-	document.getElementById("sum").innerHTML = num1+num2;
+	document.getElementById("Sum").innerHTML = num1+num2;
 }
 
